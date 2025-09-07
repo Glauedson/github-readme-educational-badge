579a13d2baddbdb63532c4a838abae0d69bec0f9
Glauedson <glauedson18s@gmail.com>
2025-09-07 16:31:38 -0300

    feat: add degree detail to badge design and fix proportions

--- src/templates/badge.svg.js
+++ src/templates/badge.svg.js
@@ -1,49 +1,53 @@
+
 export function badgeSvg({ name, course, degree, progress, img }) {
   return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="180" >
+    <svg xmlns="http://www.w3.org/2000/svg" width="410" height="173" >
         <style>
             text {
             font-family: 'Poppins', sans-serif;
             }
         </style>  
-    
-        <defs>
-            <filter id="shadow" x="0" y="0" width="250%" height="250%">
-                <feDropShadow dx="0" dy="0" stdDeviation="8" flood-color="rgba(3, 3, 3, 0.57)" />
-            </filter>
-        </defs>
 
-      <rect x="0" y="0" width="400" height="145" rx="8" ry="8" fill="#FFFFFF" filter="url(#shadow)"/>
+      <defs>
+            <filter id="shadow" x="0" y="0.1" width="100%" height="100%">
+                <feDropShadow dx="0" dy="0" stdDeviation="6" flood-color="rgba(3, 3, 3, 0.18)" />
+            </filter>
+      </defs>
+      <rect x="0" y="15" width="400" height="145" rx="8" ry="8" fill="#ffffffff" filter="url(#shadow)"/>
       
+      <rect x="270" y="0" width="140" height="35" rx="15" ry="15" fill="#D9D9D9"/>
+      <image href="https://cdn-icons-png.flaticon.com/512/43/43298.png" x="285" y="8" width="20" height="20" />
+      <text x="320" y="22" font-size="12" font-weight="regular" fill="#000000ff">${degree}</text>
+
       <!-- University logo -->
       ${img ? `<image 
         href="${img}" 
-        x="0" y="0" 
+        x="0" y="15" 
         width="145" height="145" 
         preserveAspectRatio="xMidYMid slice" 
       />` : ""}
 
       <!-- Main text -->
-      <text x="160" y="25" font-size="10" font-weight="regular" fill="#424242">CURSO</text>
-      <text x="160" y="42" font-size="14" font-weight="regular" fill="#000000ff">${course}</text>
+      <text x="160" y="40" font-size="10" font-weight="regular" fill="#424242">CURSO</text>
+      <text x="160" y="57" font-size="14" font-weight="regular" fill="#000000ff">${course}</text>
 
-      <text x="160" y="63" font-size="10" font-weight="regular" fill="#424242">FACULDADE</text>
-      <text x="160" y="80" font-size="14" font-weight="regular" fill="#000000ff">${name}</text>
+      <text x="160" y="78" font-size="10" font-weight="regular" fill="#424242">FACULDADE</text>
+      <text x="160" y="95" font-size="14" font-weight="regular" fill="#000000ff">${name}</text>
 
 
       <!-- Texto -->
-      <text x="160" y="110" font-family="Poppins, sans-serif" font-size="10" fill="#333">
+      <text x="160" y="125" font-family="Poppins, sans-serif" font-size="10" fill="#333">
         CONCLUSÃO:
       </text>
-      <text x="375" y="110" font-family="Poppins, sans-serif" font-size="10" fill="#333" text-anchor="end">
+      <text x="375" y="125" font-family="Poppins, sans-serif" font-size="10" fill="#333" text-anchor="end">
         ${progress}
       </text>
 
       <!-- Barra de fundo -->
-     <rect x="160" y="120" width="215" height="5" fill="#ddd" rx="2.5" ry="2.5" />
+     <rect x="160" y="135" width="215" height="5" fill="#ddd" rx="2.5" ry="2.5" />
 
      <!-- Barra de progresso -->
-     <rect x="160" y="120" width="200" height="5" fill="#4CAF50" rx="2.5" ry="2.5" />
+     <rect x="160" y="135" width="200" height="5" fill="#4CAF50" rx="2.5" ry="2.5" />
      </svg>
   `;
 }
